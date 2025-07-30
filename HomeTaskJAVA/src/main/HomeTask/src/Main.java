31bc7dc82837283f9e702278f1122a4e821971c7
Masha864 <mfriday84@gmail.com>
2025-07-31 01:34:16 +0300

    Add files via upload

---
+++ HomeTaskJAVA/src/main/HomeTask/src/Main.java
@@ -0,0 +1,233 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
+
+// Не смогла сделать все задания и с методами проблемы возникли, отрабатывало только с "public static void main(String[] args)" все
+
+
+
+
+
+//          * 1 задача
+
+//     public static void printThreeWords (String[]args){
+//            String words = new String[]{"Apple", "Orange", "Banana"};
+//            System.out.println(words);
+//
+//        }
+
+
+//            *2 задача
+
+//     public static void checkSumSign() {
+//
+//    int a = 10;
+//    int b = 20;
+//    int sum = a + b;
+//
+//    } if(sum >=0) {
+//
+//        System.out.println("Сумма положительная");
+//     } else {
+//        System.out.println("Сумма отрицательная");
+//       }
+
+
+
+//             * 3 задача
+
+//      public static void printColor (String[] args) {
+//            int value = 5;
+//            if (value <= 0) {
+//                System.out.println("Красный");
+//            }
+//            else if  (value > 0) {
+//                System.out.println("Желтый");
+//            }
+//            else if (value <= 100) {
+//                System.out.println ("Желтый");
+//         }
+//            else if (value > 100) {
+//                System.out.println("Зеленый");
+//     }
+
+
+//           * 4 задача
+//
+//      public static void compareNumbers() {
+//           int a = 4;
+//           int b = 8;
+//            if (a >= b) {
+//                return a;
+//            }
+//                return b;
+//
+//        }
+
+
+//             *5 задача
+
+//        static boolean doFive(int a, int b) {
+//
+//        int sum = a + b;
+//        if (sum >=10 && sum <=20)
+//
+//        return true;
+//        else return false;
+//    }
+
+
+//             *6 задача
+
+//           static void doSix (int a) {
+//
+//           if (a < 0) {
+//                System.out.println("Число " + a + "отрицательное");
+//            } else {
+//                System.out.println("Число " + a + "положительное");
+//              }
+//              }
+
+
+//                   *9 задача
+
+//        static void doNine ( int year){
+//
+//            if (!(year % 4 == 0) || ((year % 100 == 0) && !(year % 400 == 0)))
+//                System.out.println(year + " г. не високосный");
+//            else System.out.println(year + " г. високосный");
+//
+//        }
+
+
+//                *10 задача
+//
+//      public static void doTen(int[] replaceValues) {
+//        for (int i = 0; i < replaceValues.length; i++) {
+//          replaceValues[i] = (replaceValues[i] > 0)? 0 : 1;
+//           System.out.print(replaceValues[i] + " ");
+//        }
+//      }
+
+
+
+//                 *11 задача
+//
+//public static void doEleven(int size) {
+//    int[] fillArr = new int[size];
+//    for (int i = 0; i < fillArr.length; i++) {
+//        fillArr[i] = i + 1;
+//        System.out.print(fillArr[i] + " ");
+//    }
+//}
+
+
+
+
+//             * 12 задача
+//
+//   static int[] changeArray(int[] array) {
+//     for (int i = 0; i < array.length; i++)
+//        if (array[i] < 6)
+//            array[i] *= 2;
+//     return array;
+//}
+
+
+//          *13 задача
+//
+//     public static void doThirteen() {
+//       int[][] arr = new int[3][3];
+//       for (int i = 0; i < arr.length; i++) {
+//        for (int j = 0, x = arr[i].length - 1; j < arr[i].length; j++, x--) {
+//            if (i == j || i == x) arr[i][j] = 1;
+//            else arr[i][j] = 0;
+//            System.out.print(arr[i][j] + " ");
+//        }
+//             System.out.print("\n");
+//    }
+//}
+
+
+
+//          *14 задача
+//
+//       public static void doFourteen(int len, int initialValue){
+//         int[] arr = new int[len];
+//         for (int i = 0; i < len; i++) {
+//          arr[i] = initialValue;
+//           System.out.print("[" + i + "]" + arr[i] + " ");
+//         }
+//       }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
